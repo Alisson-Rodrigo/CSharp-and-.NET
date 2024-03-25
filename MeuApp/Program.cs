0223857b96799b64f8565aec1835f3db02a454ad
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-03-25 17:37:45 -0300

    inicio

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        // Solicita ao usuário que insira seu nome
-        Console.WriteLine("Digite seu nome:");
-        string nome = Console.ReadLine();
 
-        // Sauda o usuário
-        Console.WriteLine($"Olá, {nome}! Bem-vindo ao mundo da programação em C#!");
-    }
+namespace MeuApp {
+    class Program
+    {
+        static void Main()
+        {
+            // Solicita ao usuário que insira seu nome
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+
+            // Sauda o usuário
+            Console.WriteLine($"Olá, {nome}! Bem-vindo ao mundo da programação em C#!");
+        }
+    } 
 }
