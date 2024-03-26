6bc72011c7ac2fe91f38b6a542dfcec1a0966eda
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-03-25 21:37:41 -0300

    FF

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using EasyAutomationFramework;
+using OpenQA.Selenium;
 
 namespace MeuApp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Olá, Mundo!");
+            // Instanciando o driver
+            var driver = new Web();
+
+            driver.StartBrowser();
+            driver.Navigate("https://www.google.com");
+            driver.AssignValue(TypeElement.Name, "q", "oi").element.SendKeys(Keys.Enter);
+            driver.CloseBrowser();
+
         }
     }
 }
